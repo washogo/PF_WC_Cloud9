2bd471d07aed010369ae3b05e173adeec7713706
washogo <aiz4338150@gmail.com>
2021-12-31 04:06:25 +0000

    [Add]devise使用で顧客と管理者のコントローラを作成[Fix]コントローラの::::とルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :admins, controllers: {
+  devise_for :admins, path: 'admin', controllers: {
     sessions: 'admin/sessions'
   }
   devise_for :customers, controllers: {
