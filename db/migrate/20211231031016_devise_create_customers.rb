352ffc91f19f8547fb2271b6d0e1e5adb4389576
washogo <aiz4338150@gmail.com>
2021-12-31 03:50:58 +0000

    [Add]顧客と管理者のそれぞれのテーブル作成

--- db/migrate/20211231031016_devise_create_customers.rb
+++ db/migrate/20211231031016_devise_create_customers.rb
@@ -4,8 +4,20 @@ class DeviseCreateCustomers < ActiveRecord::Migration[5.2]
   def change
     create_table :customers do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string  :email,              null: false, default: ""
+      t.string  :encrypted_password, null: false, default: ""
+      t.string  :last_name,          null: false
+      t.string  :first_name,         null: false
+      t.string  :last_name_kana,     null: false
+      t.string  :first_name_kana,    null: false
+      t.string  :postal_code,        null: false
+      t.string  :address,            null: false
+      t.string  :phone_number,       null: false
+      t.string  :transfer_target,    null: false
+      t.string  :nickname,           null: false
+      t.text    :profile,            null: false
+      t.string  :image_id,           null: false
+      t.boolean :is_deleted,         null: false, default: false
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_30_144033) do
+ActiveRecord::Schema.define(version: 2021_12_31_031337) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "name", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.string "phone_number", null: false
+    t.string "transfer_target", null: false
+    t.string "nickname", null: false
+    t.text "profile", null: false
+    t.string "image_id", null: false
+    t.boolean "is_deleted", default: false, null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
 
   create_table "lessons", force: :cascade do |t|
     t.integer "category_id", null: false
