dd27634538b609bd9a3ba461ddd885324bb9aac8
washogo <aiz4338150@gmail.com>
2021-12-31 03:31:56 +0000

    [Add]管理者と顧客でdeviseを使用してモデル作成とルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {
+    sessions: 'admin/sessions'
+  }
+  devise_for :customers, controllers: {
+    registrations: 'public/registrations',
+    sessions:      'public/sessions'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope module: :public do
     resources :lessons, except:[:destroy]
