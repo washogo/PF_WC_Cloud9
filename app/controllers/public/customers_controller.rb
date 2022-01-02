76fa795b784dc71f1709073c7dd27fc6c35f4ad9
washogo <aiz4338150@gmail.com>
2022-01-02 12:17:35 +0000

    [Finish]顧客の退会確認画面の作成[Add]退会処理を作成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -24,15 +24,20 @@ class Public::CustomersController < ApplicationController
   end
 
   def cancel
+    @customer=current_customer
   end
 
   def quit
+    @customer=current_customer
+    @customer.is_deleted=params[:customer][:is_deleted]
+    @customer.update(customer_params)
+    redirect_to root_path
   end
 
   private
 
   def customer_params
-    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :nickname, :profile, :image, :transfer_target)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :nickname, :profile, :image, :transfer_target, :is_deleted)
   end
 
 
