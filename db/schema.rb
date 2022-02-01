818d3602f821eddbfd1e1d4422445db7bef08da2
washogo <aiz4338150@gmail.com>
2022-02-01 14:45:00 +0000

    customers.profileをtext型に修正してdatabase.ymlをstrictモードに戻す

--- db/schema.rb
+++ db/schema.rb
@@ -53,6 +53,30 @@ ActiveRecord::Schema.define(version: 2022_01_19_081358) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "customers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.string "phone_number", null: false
+    t.string "transfer_target"
+    t.string "nickname", null: false
+    t.string "profile"
+    t.string "image_id"
+    t.boolean "is_deleted", default: false, null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
   create_table "evaluations", force: :cascade do |t|
     t.integer "customer_id", null: false
     t.integer "evaluation", null: false
