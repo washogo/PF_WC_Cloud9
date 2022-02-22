16dbebd6b2dacb8b56fe6770135db5b66303f4ee
washogo <aiz4338150@gmail.com>
2022-02-22 03:18:09 +0000

    [Fix]サイドバーに表示されるカテゴリーに紐づく講座の数の表示を修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,6 +31,5 @@ module ApplicationHelper
   def update_at_date(updated_at, multiple)
     updated_at + 24*60*60 * multiple*30
   end
-    
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -56,3 +56,152 @@ Customer.create!(
  profile: "昭和生まれの翔です。",
 )
 
+Address.create!(
+ customer_id: 1,
+ name: "令和道子",
+ postal_code: "1111111",
+ address: "東京都渋谷区１丁目",
+)
+
+Address.create!(
+ customer_id: 2,
+ name: "平成道夫",
+ postal_code: "2222222",
+ address: "大阪府大阪市北区2丁目",
+)
+
+Address.create!(
+ customer_id: 3,
+ name: "昭和翔",
+ postal_code: "3333333",
+ address: "福岡県福岡市博多区３丁目",
+)
+
+Category.create!(
+ name: "Ruby" 
+)
+
+Category.create!(
+ name: "プログラミング" 
+)
+
+Category.create!(
+ name: "JavaScript" 
+)
+
+Category.create!(
+ name: "React.js" 
+)
+
+Category.create!(
+ name: "Ruby on Rails" 
+)
+
+Category.create!(
+ name: "英語" 
+)
+
+Category.create!(
+ name: "英会話" 
+)
+
+Category.create!(
+ name: "TOEIC" 
+)
+
+Comment.create!(
+ customer_id: 1,
+ comment: "プログラミングは難しいですか？",
+)
+
+Comment.create!(
+ customer_id: 2,
+ comment: "プログラミングは楽しいよ！",
+)
+
+Comment.create!(
+ customer_id: 3,
+ comment: "好きになれたら強いねー",
+)
+
+Comment.create!(
+ customer_id: 2,
+ comment: "英語楽しい！！",
+)
+
+Comment.create!(
+ customer_id: 3,
+ comment: "英語は何を勉強しているんですか？",
+)
+
+Comment.create!(
+ customer_id: 2,
+ comment: "TOEICだよー",
+)
+
+Comment.create!(
+ customer_id: 1,
+ comment: "やっぱり楽しいと学びが加速しますよね！",
+)
+
+Comment.create!(
+ customer_id: 3,
+ comment: "好きなプログラミング言語は何ですか？",
+)
+
+Comment.create!(
+ customer_id: 1,
+ comment: "Rubyが好きです！",
+)
+
+Comment.create!(
+ customer_id: 2,
+ comment: "バックエンドならPHP,フロントエンドならJavaScriptだねー",
+)
+
+ReplyRelationship.create!(
+ main_comment_id: 1,
+ reply_id: 2,
+)
+
+ReplyRelationship.create!(
+ main_comment_id: 1,
+ reply_id: 3,
+)
+
+ReplyRelationship.create!(
+ main_comment_id: 4,
+ reply_id: 5,
+)
+
+ReplyRelationship.create!(
+ main_comment_id: 5,
+ reply_id: 6,
+)
+
+ReplyRelationship.create!(
+ main_comment_id: 4,
+ reply_id: 7,
+)
+
+ReplyRelationship.create!(
+ main_comment_id: 8,
+ reply_id: 9,
+)
+
+ReplyRelationship.create!(
+ main_comment_id: 8,
+ reply_id: 10,
+)
+
+Lesson.create!(
+ customer_id: 3,
+ category_id: 1,
+ name: "Rubyの基礎を学ぼう！",
+ content: "Rubyの基礎を楽しく学ぶことができます！",
+ attending_style: 1,
+ access: "福岡県福岡市",
+ contract_period: 1,
+ price: 10000,
+ is_in_lecture: true,
+)
