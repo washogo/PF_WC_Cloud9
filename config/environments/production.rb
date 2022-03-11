03a839008cfd7a1e9971e3395edc134d0abf65fe
washogo <aiz4338150@gmail.com>
2022-03-11 08:02:32 +0000

    [Fix]メール通知の設定に環境変数を設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
+
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
