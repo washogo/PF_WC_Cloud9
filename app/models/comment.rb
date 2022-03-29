1e8ca67ebf83734365aa6cfac60393837265a7f2
washogo <aiz4338150@gmail.com>
2022-03-29 11:36:26 +0000

    [Fix]ommentモデルの修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :customer
 
-  has_one :replying_relationships, class_name: "ReplyRelationship", foreign_key: "reply_id", dependent: :destroy
-  has_one :replying, through: :replying_relationships, source: :main_comment
+  has_one :replying_relationship, class_name: "ReplyRelationship", foreign_key: "reply_id", dependent: :destroy
+  has_one :replying, through: :replying_relationship, source: :main_comment
 
   has_many :replied_relationships, class_name: "ReplyRelationship", foreign_key: "main_comment_id", dependent: :destroy
   has_many :replied, through: :replied_relationships, source: :reply
--- app/models/reply_relationship.rb
+++ app/models/reply_relationship.rb
@@ -1,5 +1,4 @@
 class ReplyRelationship < ApplicationRecord
   belongs_to :main_comment, class_name: "Comment"
   belongs_to :reply, class_name: "Comment", dependent: :destroy
-  
 end
