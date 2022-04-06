8b9e812378ebbe7e6bf0076a7a8ac9858f3279cc
washogo <aiz4338150@gmail.com>
2022-04-06 08:49:13 +0000

    [Fix]add shipping_fee  on orders table

--- db/migrate/20220110064819_create_orders.rb
+++ db/migrate/20220110064819_create_orders.rb
@@ -4,6 +4,7 @@ class CreateOrders < ActiveRecord::Migration[5.2]
       t.integer :customer_id,    null: false
       t.integer :address_id,     null: false
       t.integer :payment_method_id, null: false
+      t.integer :shipping_fee,   null: false, default: 500
       t.integer :total_price,    null: false
       t.integer :order_status,   null: false, default: 0
 
--- db/migrate/20220110075041_remove_shipping_fee_on_addresses.rb
+++ db/migrate/20220110075041_remove_shipping_fee_on_addresses.rb
@@ -1,7 +1,6 @@
 class RemoveShippingFeeOnAddresses < ActiveRecord::Migration[5.2]
   def up
     remove_column :addresses, :shipping_fee, :integer
-    add_column :orders, :shipping_fee, :integer
     change_column :orders, :shipping_fee, :integer, null: false
   end
 end
